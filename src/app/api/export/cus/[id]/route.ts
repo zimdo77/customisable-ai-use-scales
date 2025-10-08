598c17da04db683feda0572c63f2d4ea6c7f85c6
CharlieYUnimelb <charles.young@student.unimelb.edu.au>
2025-10-09 03:52:45 +1100

    readd filler get

--- src/app/api/export/cus/[id]/route.ts
+++ src/app/api/export/cus/[id]/route.ts
@@ -4,3 +4,7 @@
 export const dynamic = "force-dynamic"
 
 import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  return NextResponse.json({ message: "Hello World" });
+}
